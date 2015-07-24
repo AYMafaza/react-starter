f009f0db8f9088106c2cedf86b1ef413976b2227
rramdat-i <rramdat@infusion.com>
2015-07-24 16:51:49 -0400

    es6 promise polyfill

--- client/data/WorkspaceFetcher.js
+++ client/data/WorkspaceFetcher.js
@@ -1,7 +1,11 @@
 import fetch from 'isomorphic-fetch';
+import es6Promise from 'es6-promise';
+
+var Promise = es6Promise.Promise;
 
 var WorkspaceFetcher = {
   fetch: function () {
+    debugger;
     return new Promise(function (resolve, reject) {
       fetch('/workspace/default', { method: 'get' })
         .then(function(response) {
